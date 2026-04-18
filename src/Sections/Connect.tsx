cebe3bdf6dcb2e905f51e5850332c6845a069ef1
John Rogee Turqueza <jrogeet@gmail.com>
2026-04-18 22:20:04 +0800

    style: update section height classes from h-section-height-base to h-hero-height for Connect, Journey, and Projects components

--- src/Sections/Connect.tsx
+++ src/Sections/Connect.tsx
@@ -1,7 +1,7 @@
 export default function Connect() {
 
     return (
-        <section className="overflow-hidden relative flex flex-col h-section-height-base border border-bd bg-cr p-section-pad">
+        <section className="overflow-hidden relative flex flex-col h-hero-height border border-bd bg-cr p-section-pad">
              <div className="tracking-wider border-[0.5px] border-acc py-[5px] px-[10px] flex gap-2 font-dm-mono text-xxs text-muted bg-abg">
                 <p className="text-acc font-med">[ 05 ]</p>
                 <span className="text-acc">::</span>
--- src/Sections/Journey.tsx
+++ src/Sections/Journey.tsx
@@ -1,7 +1,7 @@
 export default function Journey() {
 
     return (
-        <section className="overflow-hidden relative flex flex-col h-section-height-base border border-bd bg-cr p-section-pad">
+        <section className="overflow-hidden relative flex flex-col h-hero-height border border-bd bg-cr p-section-pad">
              <div className="tracking-wider border-[0.5px] border-acc py-[5px] px-[10px] flex gap-2 font-dm-mono text-xxs text-muted bg-abg">
                 <p className="text-acc font-med">[ 04 ]</p>
                 <span className="text-acc">::</span>
